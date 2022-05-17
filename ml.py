81485114c63b56b803754ffdb630ddc74f17b2f3
MagaliDrumare <mag250475@gmail.com>
2022-05-17 11:10:38 +0200

    Update ml.py

--- ml.py
+++ ml.py
@@ -17,4 +17,4 @@ text = st.text_area(label="Context")
 if (not len(text)==0) and not (len(question)==0):
     x_dict = npl_pipe(context=text,question=question
     #st.write('Answer: ',x_dict['answer'])
-    st.write(f'The answer is {x_dict['answer']})
+    st.write(f'The answer is {x_dict}')
